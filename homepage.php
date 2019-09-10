da3a0f22da60baa5de2369ff920bf1b6cb4aea10
Noah Paige <noahpaige@wustl.edu>
2019-09-09 19:33:12 -0500

    s

---
+++ homepage.php
@@ -0,0 +1,32 @@
+<?php
+#session_start();
+$userexist = "no";
+$username = (string)$_GET["username"];
+
+if (isset($_GET["username"])){
+
+    echo $username + "\n";
+    $file = fopen("users.txt","r");
+
+    while(!feof($file)){
+        $u = (string)fgets($file);
+        #echo $u;
+        echo "\n";
+        #echo "</ul>\n";
+        if ($username == $u){
+            $userexist = "yes";
+            header("Location: userpage.php");
+            exit;
+        }
+    }
+    fclose($file);
+}
+if ($userexist == "no"){
+    echo "Not Valid Username. Try Again";
+}
+
+
+#echo htmlentities($username);
+
+#CHECK IF USER IN user.txt
+?>
