36d516f1cf32a65e631fabc768fa83ba99fcf496
Noah Paige <noahpaige@wustl.edu>
2019-09-11 11:02:36 -0500

    final update

--- module2/deleteuser.php
+++ module2/deleteuser.php
@@ -42,7 +42,7 @@ if (isset($_GET["deleteuser"])){
             // print $deleteuser;  (Sanity Check)
 
             rmdir("/home/noahpaige/users/$deleteuser");
-            echo "User Deleted: $deleteuser";
+            echo htmlentities(sprintf("User Deleted: %s",$deleteuser));
             ?>
 
             
